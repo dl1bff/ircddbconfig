71b2ba69d10d1218952e2cd2e744e743f248b9f9
Michael Dirska <dl1bff@mdx.de>
2011-01-23 11:56:07 +0100

    initial version

---
+++ ircddbconfig.sh
@@ -0,0 +1,375 @@
+#!/bin/sh
+#
+#
+# ircDDB auto config script
+#
+# Copyright (C) 2011   Michael Dirska, DL1BFF (dl1bff@mdx.de)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+comment_out() {
+
+  RETVAL=0
+
+  echo "  looking for '$2' in file $1"
+
+  if grep -q "^[[:space:]]*$2" "$1"
+  then
+    echo "  commenting out '$2'"
+    echo -e ",s/^[[:space:]]*\\($2\\)/# \\1\nwq" | ed -s "$1"
+    RETVAL=1
+  else
+    echo "  '$2' not found in file $1"
+  fi
+  
+  return $RETVAL
+}
+
+transfer_value() {
+
+  RETVAL=0
+
+  if [ $# != 4 ]
+  then
+    return $RETVAL
+  fi
+
+  echo " - $1($2) -> $3($4)"
+
+  if [ ! -f "$1" ]
+  then
+    echo "   $1 does not exists"
+    return $RETVAL
+  fi
+
+  if [ ! -f "$3" ]
+  then
+    echo "   $3 does not exists"
+    return $RETVAL
+  fi
+
+  if ! grep -q "^[[:space:]]*${2}[[:space:]]*=[[:space:]]*[[:graph:]][[:graph:]]*" "$1"
+  then
+    echo "   $1 does not contain $2 property"
+    return $RETVAL
+  fi
+
+  VALUE=` gawk '
+   /^[[:space:]]*'"$2"'[[:space:]]*=[[:space:]]*[[:graph:]][[:graph:]]*/ {
+     match( $0, "^[[:space:]]*'"$2"'[[:space:]]*=[[:space:]]*([[:graph:]][[:graph:]]*)", a)
+     print a[1]
+     exit
+   } ' < "$1" `
+
+  if [ "x$VALUE" = "x" ]
+  then
+    echo "   could not read property $2 from file $1"
+    return $RETVAL
+  fi
+
+  if grep -q "^[[:space:]]*${4}[[:space:]]*=[[:space:]]*[[:graph:]][[:graph:]]*" "$3"
+  then
+    if grep -q "^[[:space:]]*${4}[[:space:]]*=[[:space:]]*${VALUE}[[:space:]]*$" "$3"
+    then
+      echo "   $3 already has ${4}=$VALUE"
+    else
+      echo "   setting ${4}=$VALUE in file $3"
+
+      echo -e ",s/^[[:space:]]*${4}[[:space:]]*=[[:space:]]*[[:graph:]][[:graph:]]*/${4}=$VALUE/\nwq" | ed -s "$3"
+      RETVAL=1
+    fi
+  else
+    echo "   adding ${4}=$VALUE to file $3"
+    echo "${4}=$VALUE" >> "$3"
+  fi
+
+  return $RETVAL
+}
+
+
+fix_dstar_gw_init_script() {
+
+  D="$1"
+  DBAK="$D".ircddb.bak
+
+  if cp "$D" "$DBAK"
+  then
+    sed '
+      /^start)$/ {
+      N
+      N
+      s/^\(start.\n    start\n\)\(    ;;\)$/\1    \/sbin\/service ircddbd start\n\2/
+      }
+      /^stop)$/ {
+      N
+      N
+      s/^\(stop.\n\)\(    stop\n    ;;\)$/\1    \/sbin\/service ircddbd stop\n\2/
+      }
+      /^restart)$/ {
+      N
+      N
+      N
+      s/^\(restart.\n\)\(    stop\n    start\n\)\(    ;;\)$/\1    \/sbin\/service ircddbd stop\n\2    \/sbin\/service ircddbd start\n\3/
+      }
+     '  < "$DBAK"  > "$D"
+  fi
+}
+
+fix_dstar_gw_init_script_debian() {
+
+  D="$1"
+  DBAK="$D".ircddb.bak
+
+  if cp "$D" "$DBAK"
+  then
+    sed '
+      /^start)$/ {
+      N
+      N
+      s/^\(start.\n    start\n\)\(    ;;\)$/\1    \/usr\/sbin\/invoke-rc.d ircddbd start\n\2/
+      }
+      /^stop)$/ {
+      N
+      N
+      s/^\(stop.\n\)\(    stop\n    ;;\)$/\1    \/usr\/sbin\/invoke-rc.d ircddbd stop\n\2/
+      }
+      /^restart)$/ {
+      N
+      N
+      N
+      s/^\(restart.\n\)\(    stop\n    start\n\)\(    ;;\)$/\1    \/usr\/sbin\/invoke-rc.d ircddbd stop\n\2    \/usr\/sbin\/invoke-rc.d ircddbd start\n\3/
+      }
+     '  < "$DBAK"  > "$D"
+  fi
+}
+
+
+if [ -x /usr/sbin/invoke-rc.d ]
+then
+  DEBIAN=1
+  RCCMD="/usr/sbin/invoke-rc.d"
+else
+  DEBIAN=0
+  RCCMD="/sbin/service"
+fi
+
+
+echo "*** ircDDB auto config script"
+
+IRCDDB_CONFIG_CHANGED=0
+
+echo ""
+echo "* looking for ircDDB parameters that can be set automatically:"
+
+transfer_value /etc/default/ircddbmhd MHEARD_UDP_PORT /etc/ircddbd/ircDDB.properties mheard_udp_port
+
+if [ "$?" = 1 ]
+then
+  IRCDDB_CONFIG_CHANGED=1
+fi
+
+transfer_value /opt/products/dstar/dstar_gw/dsipsvd/dsipsvd.conf ZR_CALLSIGN /etc/ircddbd/ircDDB.properties rptr_call
+
+if [ "$?" = 1 ]
+then
+  IRCDDB_CONFIG_CHANGED=1
+fi
+
+transfer_value /opt/ircDDB/ircDDB.properties irc_password /etc/ircddbd/ircDDB.properties irc_password
+
+if [ "$?" = 1 ]
+then
+  IRCDDB_CONFIG_CHANGED=1
+fi
+
+transfer_value /opt/products/dstar/dstar_gw/dsgwd/dsgwd.conf ZR_ADDR /etc/default/ircddbmhd ZR_ADDR
+
+if [ "$?" = 1 ]
+then
+  IRCDDB_CONFIG_CHANGED=1
+fi
+
+transfer_value /opt/products/dstar/dstar_gw/dsgwd/dsgwd.conf ZR_PORT /etc/default/ircddbmhd ZR_PORT
+
+if [ "$?" = 1 ]
+then
+  IRCDDB_CONFIG_CHANGED=1
+fi
+
+
+echo ""
+echo "* DStarMonitor config cleanup"
+
+if [ -d /opt/dstarmon ]
+then
+
+  D=/opt/dstarmon/dstarmonitor.properties
+  if [ -f $D ]
+  then
+    config_changed=FALSE
+
+    PARAM_SUFFIX=` gawk '
+	 /^[[:space:]]*LHURI[0-9]=jdbc:postgresql:.*ircddb/ { gsub("^[[:space:]]*", ""); print substr($0, 6, 1) } ' < $D `
+
+    if [ "x$PARAM_SUFFIX" != "x" ]
+    then
+	for i in $PARAM_SUFFIX
+	do
+	  comment_out $D "LHURI${i}="
+	  comment_out $D "LHDriver${i}="
+	  comment_out $D "LHParameters${i}="
+	  config_changed=TRUE
+	done
+    fi
+
+    if grep -q "^[[:space:]]*LHURI=jdbc:postgresql:.*ircddb" $D
+    then
+	comment_out $D "LHURI="
+	comment_out $D "LHDriver="
+	comment_out $D "LHParameters="
+	config_changed=TRUE
+    fi
+
+    if [ "$config_changed" = "TRUE" ]
+    then
+	echo "  the configuration file $D was changed, restarting DStarMonitor"
+	if [ -x /etc/init.d/dsm ]
+	then
+	  /etc/init.d/dsm restart
+	fi
+    else
+	echo "  no old ircDDB config lines found"
+    fi
+  else
+    echo "  file $D does not exist"
+  fi
+else
+  echo "  DStarMonitor not found"
+fi
+
+echo ""
+echo "* dstar_gw startup script cleanup"
+
+D=/etc/init.d/dstar_gw
+
+if [ -f $D ]
+then
+  config_changed=FALSE
+
+  if grep -q "^[[:space:]]*killall -q -u ircddb" $D
+  then
+    comment_out $D "killall -q -u ircddb"
+    config_changed=TRUE
+  fi
+
+  if grep -q "^[[:space:]]*[[:graph:]]*.start.sh 2>.1 > .dev.null" $D
+  then
+    comment_out $D "[[:graph:]]*.start.sh 2>.1 > .dev.null"
+    config_changed=TRUE
+  fi
+
+  if [ "$config_changed" = "FALSE" ]
+  then
+    echo "  no old ircDDB config lines found"
+  fi
+
+  if [ "$DEBIAN" = "1" ]
+  then
+    if grep -q "^    .usr.sbin.invoke-rc.d ircddbd start$" $D
+    then
+      echo "  ircddbd start/stop commands already inserted"
+    else
+      echo "  inserting ircddbd start/stop commands"
+      fix_dstar_gw_init_script_debian $D
+    fi
+  else
+    if grep -q "^    .sbin.service ircddbd start$" $D
+    then
+      echo "  ircddbd start/stop commands already inserted"
+    else
+      echo "  inserting ircddbd start/stop commands"
+      fix_dstar_gw_init_script $D
+    fi
+  fi
+
+else
+  echo "  dstar_gw startup script could not be found"
+fi
+
+echo ""
+echo "* ircDDB password"
+
+D=/etc/ircddbd/ircDDB.properties
+
+if [ -f $D ]
+then
+  if grep -q "irc_password=IRCDDB_PASSWORD" $D
+  then
+    if [ $# != 1 ]
+    then
+      echo "#######################################"
+      echo "# please run this script again with   #"
+      echo "# the ircDDB password as command line #"
+      echo "# argument:                           #"
+      echo "# /usr/sbin/ircddbconfig pAsSwOrD     #"
+      echo "#######################################"
+      exit
+    else
+      TMP=` /bin/mktemp `
+      echo "irc_password=$1" > "$TMP"
+      transfer_value "$TMP" irc_password $D irc_password
+      /bin/rm -f "$TMP"
+      IRCDDB_CONFIG_CHANGED=1
+    fi
+  else
+    echo "  a password is set in $D"
+  fi
+else
+  echo "  $D not found"
+fi
+
+echo ""
+echo "* repeater callsign check"
+
+if [ -f $D ]
+then
+  if grep -q "rptr_call=CALLSIGN" $D
+  then
+    echo "#######################################"
+    echo "# The repeater callsign could not be  #"
+    echo "# determined automatically. Please    #"
+    echo "# edit /etc/ircddbd/ircDDB.properties #"
+    echo "#######################################"
+  else
+    echo "  a repeater callsign is set in $D"
+
+    if [ "$IRCDDB_CONFIG_CHANGED" = 1 ]
+    then
+      echo "  restarting ircDDB services"
+      $RCCMD ircddbd stop
+      $RCCMD ircddbmhd stop
+      $RCCMD ircddbd start
+      $RCCMD ircddbmhd start
+    fi
+
+  fi
+else
+  echo "  $D not found"
+fi
+
+
+
+
---
+++ ircddbconfig.spec
@@ -0,0 +1,64 @@
+#
+#
+# ircDDB auto config script
+#
+# Copyright (C) 2010   Michael Dirska, DL1BFF (dl1bff@mdx.de)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+
+Name: ircddbconfig
+Version: 1.2
+Release: 4
+License: GPLv2
+Group: Applications/System
+Summary: ircDDB auto config script
+URL: http://ircddb.net
+Packager: Michael Dirska DL1BFF <dl1bff@mdx.de>
+Requires: ed >= 0.2, gawk >= 3, ircddbd, ircddbmhd, mktemp, sed
+Source0: dl1bff-ircddbd-v1.2-4-g6e5b774.tar.gz
+BuildRoot: %{_tmppath}/%{name}-root
+BuildArch: noarch
+
+%description
+This script tries to remove old versions of ircDDB from the computer and
+configures the various parts of a DSTAR repeater automatically.
+
+%prep
+%setup -n dl1bff-ircddbd-6e5b774
+
+%build
+
+%install
+rm -rf %{buildroot}
+mkdir -p %{buildroot}/%{_sbindir}
+cp %{name}.sh %{buildroot}/%{_sbindir}/%{name}
+
+%clean
+rm -rf %{buildroot}
+
+
+%files
+%attr(755,root,root) %{_sbindir}/%{name}
+
+
+%post
+echo ""
+echo "##############################################################################"
+echo "# Start the automatic configuration with this command: %{_sbindir}/%{name}"
+echo "##############################################################################"
+
+
